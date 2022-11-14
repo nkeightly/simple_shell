4886cdcd4494aa013473ba133aa9181375225cb5
Keightly Mabasa <104975766+nkeightly@users.noreply.github.com>
2022-11-14 02:08:41 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-betty-doc *.c *.h; betty *.c *.h; betty-style *.c *.h
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c && valgrind ./a.out --track-origins=yes -s --leak-check=full --show-leak-kinds=all
